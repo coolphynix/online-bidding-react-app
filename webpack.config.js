9f18999102fad4f4e7764e65b789c5a38ee9b397
Saurabh Targe <targesourabh@gmail.com>
2017-07-27 23:58:38 +0530

    Unwanted code is deleted.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,5 @@
 const path = require('path');
 
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-//   template: './client/index.html',
-//   filename: 'index.html',
-//   inject: 'body'
-// })
-
 module.exports = {
   entry: {
     'index_bundle'              : './client/index.js'
@@ -16,17 +9,10 @@ module.exports = {
     filename  : './client/dist/[name].js'
   },
 
-  // entry: './client/index.js',
-  // output: {
-    // path: path.resolve('dist'),
-    // filename: './client/dist/index_bundle.js'
-  // },
   module: {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   }
-  // ,
-  // plugins: [HtmlWebpackPluginConfig]
 }
